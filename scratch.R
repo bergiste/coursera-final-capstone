8ac11a381d6c89af23043448c7bc3ac8929d9adf
bergiste <bergiste@gmail.com>
2015-12-23 22:37:44 -0500

    Commented unused scratch stuff

--- scratch.R
+++ scratch.R
@@ -1,25 +1,25 @@
 library(tm)
 setwd("~/ACADEMICS/datascience/Final Capstone")
-us_txt <- "final/en_US"
-us_corpus <- VCorpus( DirSource(us_txt), readerControl = list( language = "en"))
-
-
-#see meta data
-#us_corpus[[1]]
-#meta(us_corpus[[1]])
-
-#Clean up corpus
-us_corpus <- tm_map(us_corpus, content_transformer(tolower))
-us_corpus <- tm_map(us_corpus, stripWhitespace)
-
-
-#see raw text
-#blogs <- lapply(us_corpus[1], as.character)
-#news <- lapply(us_corpus[2], as.character)
-#twitter <- lapply(us_corpus[3], as.character)
-tdm <- TermDocumentMatrix(us_corpus, control = list(
-    stopwords = T, removePunctuation = T
-))
+# us_txt <- "final/en_US"
+# us_corpus <- VCorpus( DirSource(us_txt), readerControl = list( language = "en"))
+# 
+# 
+# #see meta data
+# #us_corpus[[1]]
+# #meta(us_corpus[[1]])
+# 
+# #Clean up corpus
+# us_corpus <- tm_map(us_corpus, content_transformer(tolower))
+# us_corpus <- tm_map(us_corpus, stripWhitespace)
+# 
+# 
+# #see raw text
+# #blogs <- lapply(us_corpus[1], as.character)
+# #news <- lapply(us_corpus[2], as.character)
+# #twitter <- lapply(us_corpus[3], as.character)
+# tdm <- TermDocumentMatrix(us_corpus, control = list(
+#     stopwords = T, removePunctuation = T
+# ))
 
 unigram <- readRDS(file="data/final_unigram.Rda")
 bigram <- readRDS(file="data/final_bigram.Rda")
